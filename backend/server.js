45f40ee52c7705934404daf1da07856b8027abcf
Aditi Sharma <aditiSharma55555@gmail.com>
2026-05-19 12:06:59 +0530

    fix render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dns").setServers(["8.8.8.8", "8.8.4.4"]);
+
 console.log("Starting server...");
 
 const express = require("express");
@@ -13,20 +14,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/complaints", require("./routes/complaintRoutes"));
-app.use("/api/ai", require("./routes/aiRoutes"));
-app.use(require("./middleware/errorMiddleware"));
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("DB Error:", err.message));
-
 app.get("/", (req, res) => {
   res.send("Smart Complaint API Running");
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on port 5000");
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/complaints", require("./routes/complaintRoutes"));
+app.use("/api/ai", require("./routes/aiRoutes"));
+
+app.use(require("./middleware/errorMiddleware"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => {
+    console.log("DB Error:", err.message);
+    process.exit(1);
+  });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
